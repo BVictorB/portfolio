15da12eef6af26b253a85317e4c18e46692dc4bb
bvictorb <victory-boucher@hotmail.com>
2021-03-23 20:29:35 +0100

    finished up homepage, small styling improvement

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,8 @@
+import Link from 'next/link'
 import Head from 'next/head'
 import { ProjectCard, ImageCard } from '@components'
 import { getMarkdownFolder } from '@helpers'
+import styles from './Home.module.css'
 
 const Home = ({ projects, albums }) => {
   const 
@@ -14,18 +16,24 @@ const Home = ({ projects, albums }) => {
       </Head>
       <main>
         <h1>Hi, I am Victor Boucher</h1>
-        <h2>Welcome to my personal portfolio website. Click on one of the cards below to take a look at my work or get to know more about me.</h2>
-        <h3>Featured project</h3>
+        <h2>Welcome to my personal portfolio website. Click on one of the featured items or go to the projects and photography pages to take a look at my work. If you want to get to know more about me, just go to the about page!</h2>
+        <h2 className={styles.featured}>Featured project</h2>
         <ProjectCard 
           data={projectSpotlight} 
           favorite={projectSpotlight.favorite}
         />
-        <h3>Featured album</h3>
+        <Link href="/projects">
+          <a className={styles.link} title="all projects">All projects &rarr;</a>
+        </Link>
+        <h2 className={styles.featured}>Featured photography album</h2>
         <ImageCard 
           url={`/photography/${albumSpotlight.slug}`}
           image={`/albums/${albumSpotlight.slug}/main.jpg`}
           title={albumSpotlight.title}
         />
+        <Link href="/photography">
+          <a className={styles.link} title="all albums">All albums &rarr;</a>
+        </Link>
       </main>
     </>
   )
