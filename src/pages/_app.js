d7120ad065ef2b3f24234adf2532809cab7c4da1
bvictorb <victory-boucher@hotmail.com>
2021-06-22 17:41:01 +0200

    add loading bar and spinner with nprogress

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,13 @@
 import Head from 'next/head'
+import Router from 'next/router'
+import NProgress from 'nprogress'
+import '../styles/nprogress.css'
 import { Navbar, Footer } from '@components'
 import '../styles/globals.css'
+
+Router.events.on('routeChangeStart', () => NProgress.start())
+Router.events.on('routeChangeComplete', () => NProgress.done())
+Router.events.on('routeChangeError', () => NProgress.done())
 
 const App = ({ Component, pageProps }) => {
   return (
