3b031209a3d14e013fa03ee40aabcd87ea568bc0
Victor Boucher <victory-boucher@hotmail.com>
2023-11-14 23:15:16 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,5 +2,14 @@ module.exports = {
   i18n: {
     locales: ['en-US'],
     defaultLocale: 'en-US'
+  },
+  async redirects() {
+    return [
+      {
+        source: '/*',
+        destination: 'https://www.linkedin.com/in/victor-boucher-18267813b/',
+        permanent: false,
+      },
+    ];
   }
 }
